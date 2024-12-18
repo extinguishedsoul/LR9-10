2edacc5acd9b437dfeb039580cfccef0412988bf
extinguishedsoul <vilordimarianna343@gmail.com>
2024-12-18 23:39:03 +0300

    Add files via upload

---
+++ lbrtrn910tsk1.pas
@@ -0,0 +1,53 @@
+﻿program lbrtrn910tsk1;
+type TArray = array of Integer; 
+function FindFirstPositive(arr: TArray): Integer; 
+// arr формальный параметр, по значению
+// FindFirstPositive формальная функция
+var i: Integer; // Локальная переменная функции
+begin
+  for i := 0 to High(arr) do 
+  begin
+    if arr[i] > 0 then
+    begin
+      FindFirstPositive := i + 1; 
+      Exit;
+    end;
+  end;
+  FindFirstPositive := 0; // Если положительных элементов нет, локальное значение
+end;
+// Функция для нахождения номера последнего отрицательного элемента
+function FindLastNegative(arr: TArray): Integer; 
+// arr — формальный параметр, по значению
+// FindLastNegative — формальная функция
+var i: Integer; // Локальная переменная функции
+begin
+  for i := High(arr) downto 0 do
+  begin
+    if arr[i] < 0 then
+    begin
+      FindLastNegative := i + 1; 
+      Exit;
+    end;
+  end;
+  FindLastNegative := 0; // Если отрицательных элементов нет, локальное значение
+end;
+var arr: TArray; n, i: Integer; resultFirstPositive, resultLastNegative: Integer; // Глобальные переменные программы
+begin
+  Write('Введите количество элементов массива: ');
+  ReadLn(n); 
+  SetLength(arr, n); 
+  WriteLn('Введите элементы массива:');
+  for i := 0 to n - 1 do 
+  begin
+    Write('Элемент [', i + 1, ']: ');
+    ReadLn(arr[i]); // передача по индексу
+  end;
+  resultFirstPositive := FindFirstPositive(arr); 
+  // фактический вызов функции
+  // arr передается по значению
+  WriteLn('Номер первого положительного элемента: ', resultFirstPositive);
+  resultLastNegative := FindLastNegative(arr); 
+  // фактический вызов функции
+  // arr передается по значению 
+  WriteLn('Номер последнего отрицательного элемента: ', resultLastNegative);
+end.
---
+++ lbrtrn910tsk2.pas
@@ -0,0 +1,46 @@
+﻿program lbrtrn910tsk2;
+const N = 8;
+type T2DArray = array[1..N, 1..N] of Integer; T1DArray = array[1..N] of Integer;
+function ProcessArray(matrix: T2DArray): T1DArray;
+var i, j: Integer; resultArray: T1DArray; found: Boolean;
+begin
+  for i := 1 to N do
+  begin
+    found := False;
+    for j := 1 to N - 1 do
+    begin
+      if matrix[i, j] + matrix[i, j + 1] = 7 then
+      begin
+        found := True;
+        Break;
+      end;
+    end;
+    if found then
+      resultArray[i] := 1
+    else
+      resultArray[i] := -1;
+  end;
+  ProcessArray := resultArray;
+end;
+var
+  matrix: T2DArray;
+  resultArray: T1DArray;
+  i, j: Integer;
+begin
+  Randomize;
+  WriteLn('Исходный двумерный массив:');
+  for i := 1 to N do
+  begin
+    for j := 1 to N do
+    begin
+      matrix[i, j] := Random(11);
+      Write(matrix[i, j]:4);
+    end;
+    WriteLn;
+  end;
+  resultArray := ProcessArray(matrix);
+  WriteLn('Результирующий одномерный массив:');
+  for i := 1 to N do
+    Write(resultArray[i]:4);
+  WriteLn;
+end.
